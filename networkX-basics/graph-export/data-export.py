17530f753ed88d62320f61d2ba6264d5e7c0272f
katarinasupe <supe.katarina@gmail.com>
2022-10-27 16:58:48 +0200

    Update export types

--- networkX-basics/graph-export/data-export.py
+++ networkX-basics/graph-export/data-export.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 G = nx.petersen_graph()
 nx.write_adjlist(G, "networkX-basics/graph-export/data/graph.adjlist")
+nx.write_gml(G, "networkX-basics/graph-export/data/graph.gml")
+nx.write_graphml(G, "networkX-basics/graph-export/data/graph.graphml")
 
 nx.draw(G)
 plt.show()
