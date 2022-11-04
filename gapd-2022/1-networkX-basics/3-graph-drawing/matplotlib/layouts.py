f06995ba9f582964f859c795b51839b4fec849cb
katarinasupe <supe.katarina@gmail.com>
2022-11-04 16:52:23 +0100

    Update

--- gapd-2022/1-networkX-basics/3-graph-drawing/matplotlib/layouts.py
+++ gapd-2022/1-networkX-basics/3-graph-drawing/matplotlib/layouts.py
@@ -5,13 +5,13 @@ G = nx.dodecahedral_graph()
 
 # spring layout is default - position nodes using Fruchterman-Reingold force-directed algorithm.
 # Draw the graph G with Matplotlib.
-nx.draw(G)
-plt.show()
+# nx.draw(G)
+# plt.show()
 
 # shell layout - position nodes in concentric circles.
 # nx.draw(G, pos=nx.shell_layout(G))
 # plt.show()
 
 # spiral layout - position nodes in a spiral layout.
-# nx.draw(G, pos=nx.spiral_layout(G))
-# plt.show()
+nx.draw(G, pos=nx.spiral_layout(G))
+plt.show()
--- gapd-2022/1-networkX-basics/4-graph-export/data-export.py
+++ gapd-2022/1-networkX-basics/4-graph-export/data-export.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 
 G = nx.petersen_graph()
-nx.write_adjlist(G, "networkX-basics/graph-export/data/graph.adjlist")
-nx.write_gml(G, "networkX-basics/graph-export/data/graph.gml")
-nx.write_graphml(G, "networkX-basics/graph-export/data/graph.graphml")
+nx.write_adjlist(G, "gapd-2022/1-networkX-basics/4-graph-export/data/graph.adjlist")
+nx.write_gml(G, "gapd-2022/1-networkX-basics/4-graph-export/data/graph.gml")
+nx.write_graphml(G, "gapd-2022/1-networkX-basics/4-graph-export/data/graph.graphml")
 
 nx.draw(G, pos=nx.shell_layout(G), node_color="#FB6E00")
 plt.show()
